09aba314e50ae28d5c8cf61c8ad80464c6b8ee96
Gavin Fong <gavinklfong@gmail.com>
2024-02-10 17:36:59 +0000

    cleaned up

--- transaction-filter-topology/src/main/java/space/gavinklfong/demo/finance/topology/KafkaTopologyConfig.java
+++ transaction-filter-topology/src/main/java/space/gavinklfong/demo/finance/topology/KafkaTopologyConfig.java
@@ -25,22 +25,4 @@ public class KafkaTopologyConfig {
                 .peek((key, value) -> log.info("output - key: {}, value: {}", key, value), Named.as("log-output"));
     }
 
-    @Bean
-    public Function<KStream<TransactionKey, Transaction>, KStream<TransactionKey, Transaction>[]> splitTransactionByType() {
-
-        return input -> {
-            Map<String, KStream<TransactionKey, Transaction>> transactionStreamByType = input.split()
-                    .branch((k, v) -> v.getType().equals(TransactionType.TRANSFER), Branched.as("withdrawal"))
-                    .branch((k, v) -> v.getType().equals(TransactionType.WITHDRAWAL), Branched.as("withdrawal"))
-                    .branch((k, v) -> v.getType().equals(TransactionType.DEPOSIT), Branched.as("deposit"))
-                    .defaultBranch(Branched.as("other"));
-
-            return new KStream[] {
-                    transactionStreamByType.get("transfer"),
-                    transactionStreamByType.get("withdrawal"),
-                    transactionStreamByType.get("deposit"),
-                    transactionStreamByType.get("other"),
-            };
-        };
-    }
 }
